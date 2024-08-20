d61d47351cde37ab9d06f8beba18c44436dfd098
SainteCroquette <SainteCroquette@github.com>
2024-08-20 08:43:31 +0200

    :sparkles: Added settings menu (saved to local storage) + default language input

--- Readme.md
+++ Readme.md
@@ -3,4 +3,32 @@
 #### Features:
 
 - Restore a default query in the search input when adding cards to a collection
+- Specify default language when adding cards to a collection (see below for language codes)
 - Short cut to toggle/untoggle card foil status when adding cards
+
+default language and query can be set from the top of moxfield collection page (might need to reload the page first).
+Values are stored in local storage (meaning you can reload and close web page without loosing your settings).
+
+#### Language codes:
+
+These should b e static
+
+| Language            | Value  |
+|---------------------|--------|
+| English             | LD58x  |
+| Spanish             | YxlpG  |
+| French              | kAVXO  |
+| German              | YnWyB  |
+| Italian             | EJ5yA  |
+| Portuguese          | YdK6v  |
+| Japanese            | EP6jq  |
+| Korean              | LmyKq  |
+| Russian             | k71wl  |
+| Simplified Chinese  | YlgbQ  |
+| Traditional Chinese | LVN4j  |
+| Hebrew              | Y2P4X  |
+| Latin               | ErnAR  |
+| Ancient Greek       | E53lZ  |
+| Arabic              | YoDab  |
+| Sanskrit            | Yjn7Z  |
+| Phyrexian           | E6QRq  |
--- collectionHelperScript.js
+++ collectionHelperScript.js
@@ -10,7 +10,7 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     const SEARCH_INPUT_SELECTOR = '#collectionSearch';
@@ -22,11 +22,92 @@
     const TOGGLE_FOIL_SHORTCUT = event => event.key === 'End';
 
     //update this to match your default query, then reload page
-    const DEFAULT_QUERY = 's:one cn:';
+    let DEFAULT_QUERY = 's:one cn:';
+    let DEFAULT_LANGUAGE = '';
 
     console.log("Collection Helper script enabled.");
 
-    // helper function to wait for an element to appear in the DOM
+    function createSettings() {
+        const parentElement = document.createElement('div');
+        // add background color, padding and border to the parent element
+        parentElement.style.backgroundColor = 'fff';
+        parentElement.style.padding = '16px';
+        parentElement.style.border = '1px solid black';
+        parentElement.style.borderRadius = '8px';
+        parentElement.style.display = 'flex';
+        parentElement.style.flexDirection = 'column';
+        parentElement.style.gap = '8px';
+        parentElement.style.marginBottom = '16px';
+
+        // create a title element
+        const titleElement = document.createElement('h2');
+        titleElement.textContent = 'Collection helper settings';
+        parentElement.appendChild(titleElement);
+
+        // default query input
+        const defaultQueryLabel = document.createElement('label');
+        defaultQueryLabel.textContent = 'Default query:';
+        defaultQueryLabel.style.display = 'block';
+        parentElement.appendChild(defaultQueryLabel);
+        const defaultQueryInput = document.createElement('input');
+        defaultQueryInput.type = 'text';
+        defaultQueryInput.value = DEFAULT_QUERY;
+        defaultQueryInput.style.width = '100%';
+        defaultQueryInput.classList.add('form-control');
+        parentElement.appendChild(defaultQueryInput);
+
+        //default language input
+        const defaultLanguageLabel = document.createElement('label');
+        defaultLanguageLabel.textContent = 'Default language (leave empty for english:';
+        defaultLanguageLabel.style.display = 'block';
+        parentElement.appendChild(defaultLanguageLabel);
+        const defaultLanguageInput = document.createElement('input');
+        defaultLanguageInput.type = 'text';
+        defaultLanguageInput.value = DEFAULT_LANGUAGE;
+        defaultLanguageInput.style.width = '100%';
+        defaultLanguageInput.classList.add('form-control');
+        parentElement.appendChild(defaultLanguageInput);
+
+
+        //save button
+        const saveButton = document.createElement('button');
+        saveButton.textContent = 'Save';
+        saveButton.classList.add('btn', 'btn-primary');
+        saveButton.onclick = () => handleSaveSettings(defaultQueryInput, defaultLanguageInput)
+        parentElement.appendChild(saveButton);
+
+        return parentElement;
+    }
+
+    function saveSettings(settings) {
+        localStorage.setItem('collection-helper-default-query', settings.defaultQuery);
+        localStorage.setItem('collection-helper-default-language', settings.defaultLanguage);
+        loadSettings();
+    }
+
+    function loadSettings() {
+        DEFAULT_QUERY = localStorage.getItem('collection-helper-default-query') || DEFAULT_QUERY;
+        DEFAULT_LANGUAGE = localStorage.getItem('collection-helper-default-language') || DEFAULT_LANGUAGE;
+    }
+
+    function handleSaveSettings(searchQueryInput, defaultLanguageInput) {
+        const newDefaultQuery = searchQueryInput.value;
+        const newDefaultLanguage = defaultLanguageInput.value;
+
+        saveSettings({
+            defaultQuery: newDefaultQuery || DEFAULT_QUERY,
+            defaultLanguage: newDefaultLanguage || DEFAULT_LANGUAGE
+        })
+    }
+
+    function injectSettings() {
+        const settings = createSettings();
+        const originElement = document.querySelector('.container.my-5');
+        //insert as first element
+        originElement.insertBefore(settings, originElement.firstChild);
+    }
+
+// helper function to wait for an element to appear in the DOM
     function waitForElement(query, callback, interval = 100) {
         const checkExistence = setInterval(() => {
             const element = document.querySelector(query);
@@ -37,7 +118,7 @@
         }, interval);
     }
 
-    // helper function to wait for an element to disappear from the DOM
+// helper function to wait for an element to disappear from the DOM
     function waitForElementDisappear(query, callback, interval = 100) {
         const checkExistence = setInterval(() => {
             const element = document.querySelector(query);
@@ -48,7 +129,7 @@
         }, interval);
     }
 
-    //Restore the default query in the search input
+//Restore the default query in the search input
     function restoreDefaultQuery(searchInput) {
         if (searchInput) {
             searchInput.value = DEFAULT_QUERY;
@@ -74,7 +155,7 @@
         });
     }
 
-    // Toggle foil on the current modal
+// Toggle foil on the current modal
     function toggleFoil() {
         console.log('foils !!!');
         const select = document.querySelector('#finish');
@@ -89,6 +170,18 @@
         }
     }
 
+    function selectLanguage() {
+        const element = document.querySelector('#language');
+        if (element && DEFAULT_LANGUAGE !== '') {
+            element.value = DEFAULT_LANGUAGE;
+            const event = new Event('change', {
+                bubbles: true,
+                cancelable: true
+            });
+            element.dispatchEvent(event);
+        }
+    }
+
 
     function attachSearchInputDisappearListener() {
         waitForElementDisappear(SEARCH_INPUT_SELECTOR, () => {
@@ -98,18 +191,25 @@
 
     }
 
+    function attachLanguageSelectListener() {
+        waitForElement('#language', (element) => {
+            selectLanguage();
+        });
+    }
+
     function attachSearchInputAppearListener() {
         waitForElement(SEARCH_INPUT_SELECTOR, (element) => {
             console.log('Search input found:', element);
             restoreDefaultQuery(element);
             attachSaveButtonAppearsListener();
+            attachLanguageSelectListener();
             attachSearchInputDisappearListener();
         });
 
     }
 
     function attachShortcutsListeners() {
-        document.addEventListener('keydown', function(event) {
+        document.addEventListener('keydown', function (event) {
             if (TOGGLE_FOIL_SHORTCUT(event)) {
                 event.preventDefault();
                 toggleFoil();
@@ -119,4 +219,10 @@
 
     attachShortcutsListeners();
     attachSearchInputAppearListener();
-})();
+    loadSettings();
+    const timeout = setTimeout(() => {
+        injectSettings();
+        clearTimeout(timeout);
+    }, 3000);
+})
+();
